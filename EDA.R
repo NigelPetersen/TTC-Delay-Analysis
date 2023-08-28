806ab3a24a5190e03284fac265c045636d3e5f19
Nigel Petersen <107229019+NigelPetersen@users.noreply.github.com>
2023-08-28 12:10:09 -0400

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(here)
 library(bayesplot)
 library(loo)
-library(rear)
+library(readr)
 
 Delays_2023 = read.csv(here("Github", "TTTC-Delay-Analysis", 
                "cleaned_delays_2023.csv"))
