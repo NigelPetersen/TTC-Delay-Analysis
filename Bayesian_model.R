75055fcd2c54bfe367163a69d94af464d60a0bb9
Nigel Petersen <nigelpetersen611@gmail.com>
2023-09-02 13:25:38 -0400

    Update Bayesian_model.R

--- Bayesian_model.R
+++ Bayesian_model.R
@@ -1,22 +1,33 @@
 source(here("Github", "TTTC-Delay-Analysis", "EDA.R"))
 
-Delays_2023 <- mutate_at(c("DayOfMonth", "Vehicle"), as.factor)
+100*(nrow(Delays_2023 |> filter(Min.Delay > 120))/nrow(Delays_2023))
+Delays_2023 <- Delays_2023 |> filter(Min.Delay <= 120)
 
 full_model <- glm(Min.Delay ~ ., data = Delays_2023, family = "poisson")
 summary(full_model)
 
-full_res <- full_model$residuals
+full_model <- step(full_model, trace = 0, direction = "backward")
+
 full_fit <- full_model$fitted.values
-full_pred <- log(full_fit)
+full_res <- log((Delays_2023$Min.Delay - full_fit)^2)
 
 # Check for overdispersion
 
+halfnorm(residuals(full_model))
+
 data.frame(residuals = full_res, fitted = full_fit) |>
   ggplot() +
-  geom_point(aes(x = fitted, y = log(residuals))) +
+  geom_point(aes(x = fitted, y = residuals)) +
   labs(x= "Fitted values", y = "Residuals", 
   title = "Residuals vs Fitted values") +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5)) +
-  geom_hline(yintercept = 0)
+  geom_abline(slope = 1,yintercept = 0)
+
+#adjust standard errors using overdispersion factor
+
+disp_factor <- sum(residuals(full_model, type = "pearson")^2)/full_model$df.residual
+summary(full_model, dispersion= disp_factor)
+
+
 
